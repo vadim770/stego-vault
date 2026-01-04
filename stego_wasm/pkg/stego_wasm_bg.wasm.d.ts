44eaba413310cbcc4edd2b727b8d01fcdbcb2ba6
naxash <vadimetlin@gmail.com>
2026-01-04 16:02:17 +0200

    update

---
+++ stego_wasm/pkg/stego_wasm_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_decryptedfile_free: (a: number, b: number) => void;
+export const calculate_required_pixels: (a: number, b: number, c: number, d: number) => number;
+export const decrypt_and_extract: (a: number, b: number, c: number, d: number) => [number, number, number];
+export const decryptedfile_data: (a: number) => [number, number];
+export const decryptedfile_filename: (a: number) => [number, number];
+export const encrypt_and_hide: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number) => [number, number, number, number];
+export const preview_heatmap: (a: number, b: number, c: number, d: number, e: number) => [number, number, number, number];
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
